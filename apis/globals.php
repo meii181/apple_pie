b86dd660979aaa4ac22b2a81e949d9451d7390a9
Filoftea-Bianca <myangeldankouzo00@gmail.com>
2024-04-20 16:42:24 +0200

    changes in globals.php

--- apis/globals.php
+++ apis/globals.php
@@ -7,15 +7,20 @@ define('_PASSWORD_MAX_LEN', 15);
 define('_ITEM_MIN_LEN', 5);
 define('_ITEM_MAX_LEN', 20);
 
-function _db(){
+function _db()
+{
 
-  $database_user_name = "root";
-  $database_password = "";
-  $database_connection = 'mysql:host=localhost; dbname=amazorful; charset=utf8mb4';
-  
+  $database_url = parse_url(getenv("JAWSDB_URL"));
+  $database_user_name = $database_url["user"];
+  $database_password = $database_url["pass"];
+  $database_host = $database_url["host"];
+  $database_db = ltrim($database_url["path"], '/');
+
+  $database_connection = "mysql:host={$database_host}; dbname={$database_db}; charset=utf8mb4";
+
   $database_options = [
-      PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION,
-      PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
   ];
   return new PDO($database_connection, $database_user_name, $database_password, $database_options);
-  }
+}
